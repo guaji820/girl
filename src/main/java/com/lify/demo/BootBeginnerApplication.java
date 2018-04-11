47bd73263a5c2cb64103d2b64f801ea3bffc96ef
李方园 <thllifangyuan@163.com>
2018-04-11 14:26:58 +0800

    init

---
+++ src/main/java/com/lify/demo/BootBeginnerApplication.java
@@ -0,0 +1,12 @@
+package com.lify.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBeginnerApplication.class, args);
+    }
+}
